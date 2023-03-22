dda3f08513586f1b6bc28e8085433f356d4dd359
chenhuan.gao <chenhuan.gao@rwth-aachen.de>
2023-03-22 17:44:57 +0000

    1. position normalization 2. angular normalization 3. use angular system (angular system.ipynb)

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import matplotlib
+
+matplotlib.use("Qt5Agg")
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import matplotlib
+
 # Fixing random state for reproducibility
 np.random.seed(19680801)
-matplotlib.use('Qt5Agg')
+
 
 def random_walk(num_steps, max_step=0.05):
     """Return a 3D random walk as (num_steps, 3) array."""
@@ -14,10 +19,11 @@ def random_walk(num_steps, max_step=0.05):
     return walk
 
 
-def update_lines(num, walk, lines):
-    # NOTE: there is no .set_data() for 3 dim data...
-    lines.set_data([num/10, num/10+0.1], [num/10, num/10+0.1])
-    lines.set_3d_properties([num/10, num/10+0.1])
+def update_lines(num, walks, lines):
+    for line, walk in zip(lines, walks):
+        # NOTE: there is no .set_data() for 3 dim data...
+        line.set_data(walk[:num, :2].T)
+        line.set_3d_properties(walk[:num, 2])
     return lines
 
 
@@ -30,11 +36,60 @@ fig = plt.figure()
 ax = fig.add_subplot(projection="3d")
 
 # Create lines initially without data
-lines = ax.plot([], [], [])[0]
+lines = [ax.plot([], [], [])[0] for _ in walks]
 
+# Setting the axes properties
+ax.set(xlim3d=(0, 1), xlabel='X')
+ax.set(ylim3d=(0, 1), ylabel='Y')
+ax.set(zlim3d=(0, 1), zlabel='Z')
 
 # Creating the Animation object
 ani = animation.FuncAnimation(
-    fig, update_lines, num_steps, fargs=(, lines), interval=100)
+    fig, update_lines, num_steps, fargs=(walks, lines), interval=100)
+
+is_paused = False
+def toggle_pause(ani):
+    global is_paused
+    if is_paused:
+        ani.resume()
+    else:
+        ani.pause()
+    is_paused = not is_paused
+
+
+fig.canvas.mpl_connect('button_press_event', lambda event: toggle_pause(ani))
 
 plt.show()
+
+class PauseAnimation:
+    def __init__(self):
+        fig, ax = plt.subplots()
+        ax.set_title('Click to pause/resume the animation')
+        x = np.linspace(-0.1, 0.1, 1000)
+
+        # Start with a normal distribution
+        self.n0 = (1.0 / ((4 * np.pi * 2e-4 * 0.1) ** 0.5)
+                   * np.exp(-x ** 2 / (4 * 2e-4 * 0.1)))
+        self.p, = ax.plot(x, self.n0)
+
+        self.animation = animation.FuncAnimation(
+            fig, self.update, frames=200, interval=50, blit=True)
+        self.paused = False
+
+        fig.canvas.mpl_connect('button_press_event', self.toggle_pause)
+
+    def toggle_pause(self):
+        if self.paused:
+            self.animation.resume()
+        else:
+            self.animation.pause()
+        self.paused = not self.paused
+
+    def update(self, i):
+        self.n0 += i / 100 % 5
+        self.p.set_ydata(self.n0 % 20)
+        return (self.p,)
+
+
+pa = PauseAnimation()
+#plt.show()
