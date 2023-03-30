619b38859a50c4a3d2bec701bedc4bac6041a277
chenhuan.gao <chenhuan.gao@rwth-aachen.de>
2023-03-30 13:53:13 +0100

    add a file for generation

--- test.py
+++ test.py
@@ -1,10 +1,44 @@
 import numpy as np
-from scipy.spatial.distance import euclidean
+import pandas as pd
+import math
 
-from fastdtw import fastdtw
 
-x = np.array([[1, 1, 1], [2, 2, 2], [3, 3, 3], [4, 4, 5], [5, 5, 5]])
-y = np.array([[2, 2, 2], [3, 3, 3], [4, 4, 4]])
-distance, path = fastdtw(x, y, dist=euclidean)
-print(distance)
-print(path)
+DATA_PATH = "C:\\Users\\gaoch\\MA\\Badminton weights\\P01_B\\"
+
+SHEET_NAME = ["Segment Angular Velocity"]
+
+SIMPLIFIED_JOINTS = ["Head", "Neck", "Left Upper Arm", "Right Upper Arm", "Left Forearm", "Right Forearm",
+                     "Left Hand", "Right Hand", "Pelvis", "Left Upper Leg", "Right Upper Leg",
+                     "Left Lower Leg", "Right Lower Leg", "Left Foot", "Right Foot"]
+
+SKELETON_CONNECTION_MAP = [["Head", "Neck"],
+                         ["Neck", "Left Upper Arm"],
+                         ["Left Upper Arm", "Left Forearm"],
+                         ["Left Forearm", "Left Hand"],
+                         ["Neck", "Right Upper Arm"],
+                         ["Right Upper Arm", "Right Forearm"],
+                         ["Right Forearm", "Right Hand"],
+                         ["Neck", "Pelvis"],
+                         ["Pelvis", "Left Upper Leg"],
+                         ["Left Upper Leg", "Left Lower Leg"],
+                         ["Left Lower Leg", "Left Foot"],
+                         ["Pelvis", "Right Upper Leg"],
+                         ["Right Upper Leg", "Right Lower Leg"],
+                         ["Right Lower Leg", "Right Foot"]]
+
+used_cols = []
+for joints in SIMPLIFIED_JOINTS:
+    used_cols.append(joints+" x")
+    used_cols.append(joints+" y")
+    used_cols.append(joints+" z")
+
+def read_data(file_name):
+    return pd.read_excel(DATA_PATH + file_name,
+                         sheet_name = SHEET_NAME,
+                         usecols = used_cols)
+
+
+motion_recording = read_data("P01_B_noweightwithgloves.xlsx")
+
+motion_recording = motion_recording[5000:10000]
+motion_recording.to_csv("angular velocity", index = False)
